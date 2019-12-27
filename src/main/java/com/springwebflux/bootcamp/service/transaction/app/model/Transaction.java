59fafddd736e2af3d0b541e61dcde80b759bc58e
hernanspc <iphone1198@icloud.com>
2019-12-27 18:26:53 -0500

    add

--- src/main/java/com/springwebflux/bootcamp/service/transaction/app/model/Transaction.java
+++ src/main/java/com/springwebflux/bootcamp/service/transaction/app/model/Transaction.java
@@ -1,15 +1,17 @@
 package com.springwebflux.bootcamp.service.transaction.app.model;
-
+import java.util.Date;
+ 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="transaction")
 public class Transaction {
-
-@Id
+	
+@Id 
 private String id;
 private String nrodoc;
 private String tip_acc;
-private 
+private Date fec_operation;
 
 }
